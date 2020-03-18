1ef364e495b80c71c21181a730ffbd69cc426218
marcellocurto <marcello@roark.at>
2020-03-18 18:56:32 +0100

    Ajax Scroll v3

--- src/js/js-init.js
+++ src/js/js-init.js
@@ -1,13 +1,14 @@
-var ias = jQuery.ias({
-  container: 'main',
+let ias = new InfiniteAjaxScroll('main', {
   item: 'article',
-  pagination: '.pagination',
   next: '.next',
-  negativeMargin: 1900,
+  pagination: '.pagination',
+  negativeMargin: 1000,
+  logger: false
 });
 
-ias.extension(new IASPagingExtension());
-
-ias.extension(new IASHistoryExtension({
-  prev: '.previous',
-}));
+ias.on('page', (event) => {
+  let state = history.state;
+  history.replaceState(state, event.title, event.url);
+  console.log(state, event.title, event.url);
+  
+});
